d68fb074e61c3c2a8d9b763e2dee08fe2e53faa4
rasberryrabbit <purplecofe2@gmail.com>
2025-08-27 00:54:20 +0900

    Refine code.

--- python_random_mp3/code.py
+++ python_random_mp3/code.py
@@ -31,8 +31,27 @@ led = neopixel.NeoPixel(board.NEOPIXEL, 1)
 # uart tx = GP4, rx = GP5
 uart1 = busio.UART(board.GP4, board.GP5, baudrate=9600)
 
+ucmd=0
+udata=0
+
+async def uart_reader(uart):
+    global ucmd, udata, led
+    while True:
+        if uart.in_waiting>=10:
+            buf=uart.read(10)
+            if buf:
+                if buf[0]==0x7e:
+                    ucmd=buf[3]
+                    udata=struct.unpack('>h',buf[5:7])[0]
+                    print(hex(ucmd), hex(udata))
+                else:
+                    led[0]=(0,0,50)
+                    time.sleep(0.02)
+                    led[0]=(0,0,0)
+        await asyncio.sleep(0.01)
+
 def dfp_write_data(cmd, dataL=0, dataH=0):
-    uart1.reset_input_buffer()
+    global ucmd
     checksum=0xffff-(0xff+0x06+cmd+0x00+dataH+dataL)+1
     cksum=checksum.to_bytes(2,'big')
     uart1.write(b'\x7E')        # Start
@@ -44,6 +63,8 @@ def dfp_write_data(cmd, dataL=0, dataH=0):
     uart1.write(bytes([dataL])) # DataL
     uart1.write(cksum)          # checksum High
     uart1.write(b'\xEF')        # Stop
+    uart1.reset_input_buffer()
+    ucmd=0
 
     # give device some time
     if cmd == 0x09:                        # set_media
@@ -54,30 +75,20 @@ def dfp_write_data(cmd, dataL=0, dataH=0):
       time.sleep(0.500)
     else:
       time.sleep(0.100)            # other commands
-      
-def dfp_read_data():
-    outtime = ticks_add(ticks_ms(), 1000)
-    while uart1.in_waiting<10:
-        time.sleep(0.03)
-        if not ticks_less(ticks_ms(), outtime):
-            return None
 
+
+def dfp_read_data():
+    outtime = ticks_add(ticks_ms(), 300)
+    while uart1.in_waiting<10:
+        time.sleep(0.01)
+        if not ticks_less(ticks_ms(), outtime):
+            if uart1.in_waiting==0:
+                return None
     buf=uart1.read(10)
     if buf:
         return struct.unpack('>h',buf[5:7])[0]
     else:
         return None
-
-def dfp_read_dummy():
-    outtime = ticks_add(ticks_ms(), 1000)
-    while uart1.in_waiting<10:
-        time.sleep(0.03)
-        if not ticks_less(ticks_ms(), outtime):
-            return
-
-    buf=uart1.read(10)
-    if buf:
-        print(buf.hex())
 
 # turn on dfplayer
 dfon.value=True
@@ -94,39 +105,42 @@ except:
 
 global nfiles
 # init dfplayer
-led[0]=(0,0,50)
-try:
-    # volume
-    dfp_write_data(cmd=0x06,dataL=vol)
-    dfp_read_dummy()
-    # file count
-    dfp_write_data(cmd=0x4e,dataL=1)
-    nfiles=dfp_read_data()
-except Exception as e:
-    nfiles=0
-    print(e)
-led[0]=(0,0,0)    
-
-print("MP3 Files : %d" % nfiles)
-print(stby.value)
-# get volume
-dfp_write_data(cmd=0x43)
-vol=dfp_read_data()
-print("volume %d" %vol)
   
 def get_delay():
     return random.randrange(180-45)+45
 
 
 # timer loop
-deadline = ticks_add(ticks_ms(), 1000)
-timeval = 0
-lastplay=0
-lastdelay=get_delay()
-print("delay %d" % lastdelay)
-
 async def main_loop():
-    global deadline, timeval, lastdelay, lastplay, led, stby, nfiles, userpin, vol
+    global led, stby, userpin, vol
+    deadline = ticks_add(ticks_ms(), 1000)
+    timeval = 0
+    lastplay=0
+
+    led[0]=(0,0,50)
+    try:
+        # volume
+        dfp_write_data(cmd=0x06,dataL=vol)
+        dfp_read_data()
+        # file count
+        dfp_write_data(cmd=0x4e,dataL=1)
+        nfiles=dfp_read_data()
+    except Exception as e:
+        nfiles=0
+        print(e)
+    led[0]=(0,0,0)    
+
+    print("MP3 Files : %d" % nfiles)
+    while not stby.value:
+        await asyncio.sleep(0.01)
+    print(stby.value)
+    # get volume
+    dfp_write_data(cmd=0x43)
+    vol=dfp_read_data()
+    print("volume %d" %vol)
+    lastdelay=get_delay()
+    print("delay %d" % lastdelay)
+
     while True:
         # interval proc
         if not ticks_less(ticks_ms(), deadline):
@@ -137,10 +151,10 @@ async def main_loop():
                         led[0]=(50,0,0)
                         # volume
                         dfp_write_data(cmd=0x06,dataL=vol)
-                        dfp_read_dummy()
+                        dfp_read_data()
                         # prevent repeat
                         dfp_write_data(cmd=0x19,dataL=0x01)
-                        dfp_read_dummy()
+                        dfp_read_data()
                         # get new MP3 track
                         nplay=lastplay
                         while lastplay==nplay:
@@ -149,7 +163,7 @@ async def main_loop():
                         # play MP3 track
                         dfp_write_data(cmd=0x14,dataL=nplay & 0xff,dataH=0x10+int(nplay/256))
                         led[0]=(0,0,0)
-                        dfp_read_dummy()
+                        dfp_read_data()
                         print("play %d" % nplay)
                 except Exception as e:
                     print(e)
@@ -169,25 +183,6 @@ async def main_loop():
         # user stop
         if not userpin.value:
             break
-        await asyncio.sleep(0.01)
-
-ucmd=0
-udata=0
-
-async def uart_reader(uart):
-    global ucmd, udata
-    while True:
-        if uart.in_waiting>=10:
-            buf=uart.read(10)
-            if buf:
-                if buf[0]==0x7e:
-                    ucmd=buf[3]
-                    udata=struct.unpack('>h',buf[5:7])[0]
-                    print(hex(ucmd), hex(udata))
-                else:
-                    led[0]=(0,0,50)
-                    time.sleep(0.02)
-                    led[0]=(0,0,0)
         await asyncio.sleep(0.01)
 
 async def main():
